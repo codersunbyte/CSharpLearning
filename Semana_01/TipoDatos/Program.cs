430ee8ab1a08922819d2f885aca516bd514dacf5
codersunbyte <dgosolar@gmail.com>
2026-08-10 17:37:59 -0400

    chore: se sube avance de semana 01 y proyecto

---
+++ Semana_01/TipoDatos/Program.cs
@@ -0,0 +1,88 @@
+﻿/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Tipos de datos y creación de variables en C#
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*
+La creación de variables sigue una sintaxis la cual consiste en lo siguiente:
+Type -> nombre variable -> Valor Variable.
+Al crear una variable no es necesario que se almacene un valor inicial.
+Ej: string soyUnEjemplo ;
+En la variable de ejemplo se crea una variable siguiendo la sintaxis pero no se inicaliza.
+
+Tambien se pueden asignar el valor de otras variables según el tipo de dato
+double precioFinal = precio1 + precio2;
+*/
+
+/*
+Enteros - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+byte example1 = 255;
+sbyte example2 = -100;
+short example3 = 30000;
+ushort example4 = 60000;
+int example5 = 1000;
+uint example6 = 4000000000U;
+long example7 = 10000000000L;
+ulong example8 = 18000000000UL;
+
+//El tipo más usado es 'int', el resto se usa dependiendo de la optimización de datos según el rango a almacenar
+//los tipos de datos con sufijo u (uint, ushort, ulong) no permiten negativos.
+//Para detalles de rango según tipo, se encuentra detallado en obsidian notes.
+
+
+/*
+Punto Flotante - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+float example9 = 3.14f;
+double example10 = 3.141592;
+/*
+* 'double' es el tipo más usado.
+* float siempre va con sufijo f, ocupa menos memoria pero es más impreciso.
+*/
+
+/*
+Decimal - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+decimal example11 = 3.141592m;
+//Ofrece mayor presición y es ideal para calculos financieros.
+
+/*
+Carácter - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+char example12 = 'A';
+//Se usa para almacenar un carácter, siempre debe ir con comillas simples a diferencia de string.
+
+/*
+String - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+string example13 = "Hola mundo";
+/*
+*Cadena de caracteres, va entre comillas doble y se puede aplicar concadenación.
+*Concadenar -> string concadenarExample= "" + "" + ""; Se usa '+' para hacer cadenas.
+*/
+
+/*
+Booleano - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+bool example14 = true;
+//valores booleanos van de true o false.
+
+
+
+/*-------------------------------------------------------------------------------------------------------*/
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Input y Output en C#
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+//Intput recolecta información de la pantalla y esa info se puede almacenar en variables
+Console.WriteLine("Ingresa tu nombre...");
+string name = Console.ReadLine();
+//los valores almacenados SIEMPRE se guarda como string, en caso se guardar otro tipo se realiza conversion.
+Console.WriteLine("Ingresa edad...");
+string txt = Console.ReadLine();
+if (int.TryParse(txt, out int edad))
+{
+    Console.WriteLine($"Tu nombre es {name} y tienes {edad} años.");
+}
+else
+{
+    Console.WriteLine("edad no valida");
+}
---
+++ Semana_01/proyecto/Calculadora/Program.cs
@@ -0,0 +1,176 @@
+﻿/*
+* En este proyecto intentare realizar una calculadora...
+*/
+
+/*- - - MENU CALCULADORA - - -*/
+/*El usuario elige la opearación a realizar entre las opciones*/
+Console.WriteLine("-.-.-.-.-.-.Bienvenido a la calculadora.-.-.-.-.-.-");
+Console.WriteLine("Seleccione alguna de las siguientes operaciones: ");
+Console.WriteLine("1. Sumar");
+Console.WriteLine("2. Restar");
+Console.WriteLine("3. Multiplicar");
+Console.WriteLine("4. Dividir");
+Console.WriteLine("5. Salir");
+Console.WriteLine();
+string opcionMenu = Console.ReadLine();
+int nroTransformado1;
+int nroTransformado2;
+int resultado;
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+/*SUMAR*/
+if (opcionMenu == "1")
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese los números a sumar...");
+    Console.Write("Número 1: ");
+    string nroSuma1 = Console.ReadLine();
+    if (int.TryParse(nroSuma1, out nroTransformado1))
+    {
+        //número convertido   
+    }
+    else
+    {
+        Console.WriteLine("Valor erroneo...");
+        Environment.Exit(0);
+    }
+
+    Console.Write("Número 2: ");
+    string nroSuma2 = Console.ReadLine();
+    if (int.TryParse(nroSuma2, out nroTransformado2))
+    {
+        //número convertido   
+    }
+    else
+    {
+        Console.WriteLine("Valor erroneo...");
+        Environment.Exit(0);
+    }
+
+
+
+    resultado = nroTransformado1 + nroTransformado2;
+    Console.WriteLine($"El resultado de la suma es {resultado}");
+    Environment.Exit(0);
+
+
+
+
+
+    /*RESTAR*/
+}
+else if (opcionMenu == "2")
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese los números a restar...");
+    Console.Write("Número 1: ");
+    string nroResta1 = Console.ReadLine();
+    if (int.TryParse(nroResta1, out nroTransformado1))
+    {
+        //tranformación
+    }
+    else
+    {
+        Console.WriteLine("Valor Erroneo...");
+        Environment.Exit(0);
+    }
+
+    Console.WriteLine("Número 2: ");
+    string nroResta2 = Console.ReadLine();
+    if (int.TryParse(nroResta2, out nroTransformado2))
+    {
+        //transformación
+    }
+    else
+    {
+        Console.WriteLine("Valor Erroneo...");
+        Environment.Exit(0);
+    }
+
+    resultado = nroTransformado1 - nroTransformado2;
+    Console.WriteLine($"El resultado de la operación es {resultado}");
+    Environment.Exit(0);
+
+
+
+
+
+
+    /*MULTIPLICAR*/
+}
+else if (opcionMenu == "3")
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese los números a multiplicar...");
+    Console.Write("Número 1: ");
+    string nroMuli1 = Console.ReadLine();
+    if (int.TryParse(nroMuli1, out nroTransformado1))
+    {
+        //Nro Transformado
+    }
+    else
+    {
+        Console.WriteLine("Valor Erroneo...");
+        Environment.Exit(0);
+    }
+
+    Console.Write("Número 2: ");
+    string nroMulti2 = Console.ReadLine();
+    if (int.TryParse(nroMulti2, out nroTransformado2))
+    {
+        //Nro Transformado
+    }
+    else
+    {
+        Console.WriteLine("Valor Erroneo...");
+        Environment.Exit(0);
+    }
+
+
+    resultado = nroTransformado1 * nroTransformado2;
+    Console.WriteLine($"El resultado de la operación es {resultado}");
+    Environment.Exit(0);
+
+
+    /*DIVIDIR*/
+}
+else if (opcionMenu == "4")
+{
+    Console.Clear();
+    Console.WriteLine("");
+    Console.Write("Número 1: ");
+    string nroDiv1 = Console.ReadLine();
+    if (double.TryParse(nroDiv1, out double nroTransformadoDiv1))
+    {
+        //Nro Transformado
+    }
+    else
+    {
+        Console.WriteLine("Valor Erroneo...");
+        Environment.Exit(0);
+    }
+
+    Console.Write("Número 2: ");
+    string nroDiv2 = Console.ReadLine();
+    if (double.TryParse(nroDiv2, out double nroTransformadoDiv2))
+    {
+        //Nro Transformado
+    }
+    else
+    {
+        Console.WriteLine("Valor Erroneo...");
+        Environment.Exit(0);
+    }
+    double resultadoDiv = nroTransformadoDiv1 / nroTransformadoDiv2;
+    Console.WriteLine($"El resultado de la operación es {resultadoDiv}");
+    Environment.Exit(0);
+    /*SALIR*/
+}
+else if (opcionMenu == "5")
+{
+    Environment.Exit(0);
+}
+else
+{
+    Console.Write("opción invalida");
+    Environment.Exit(0);
+}
